bc1c76961dfd289e0091968237405e8650371841
Venkateshkonnipati <157687031+Venkateshkonnipati@users.noreply.github.com>
2024-07-20 19:39:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const typed = new Typed(".text", {
+    strings:["Frontened Developer", "Web and App Designer", "UI/UX Designer", "Figma Designer", "Computer Science Student"],
+    typeSpeed:90,
+    backSpeed:90,
+    backDelay:1000,
+    loop: true,
+});
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
